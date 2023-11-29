56fe3c13d3c321f2bac892703dbeb318aab63ca3
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-11-29 22:30:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
 import json
 import numpy as np
 import torch
-from transformers import pipeline
-from diffusers import DiffusionPipeline
+from diffusers import AutoPipelineForText2Image
 import base64
 from io import BytesIO
 
 
 class InferlessPythonModel:
   def initialize(self):
-    self.generator = DiffusionPipeline.from_pretrained(
-      "stabilityai/stable-diffusion-xl-base-1.0",
-      torch_dtype=torch.float16,
-      use_safetensors=True,
-      variant="fp16",
-      device_map="auto"
-    )
+    self.pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16", device_map="auto")
 
   def infer(self, inputs):
     prompt = inputs["prompt"]
-    self.generator.enable_xformers_memory_efficient_attention()
-    pipeline_output_image = self.generator(prompt).images[0]
+    pipeline_output_image = self.pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
     buff = BytesIO()
     pipeline_output_image.save(buff, format="PNG")
     img_str = base64.b64encode(buff.getvalue())
