fe1afe997a263c2f5adbe9f84903bdceabbee3ee
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-06-08 13:49:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
-import json
-import numpy as np
 import torch
-from diffusers import AutoPipelineForText2Image
+from diffusers import AutoPipelineForText2Image, AutoencoderKL, EulerAncestralDiscreteScheduler
 import base64
 from io import BytesIO
 
-
 class InferlessPythonModel:
   def initialize(self):
-    self.pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16", device_map="auto")
+    vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
+    self.pipeline = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo",vae=vae, torch_dtype=torch.float16, variant="fp16",use_safetensors=True)
+    self.pipeline = self.pipeline.to("cuda")
+    self.pipeline.scheduler =  EulerAncestralDiscreteScheduler.from_config(self.pipeline.scheduler.config)
 
   def infer(self, inputs):
     prompt = inputs["prompt"]
-    pipeline_output_image = self.pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+    pipeline_output_image = self.pipeline(prompt=prompt,
+                                          num_inference_steps=1,
+                                          guidance_scale=1).images[0]
     buff = BytesIO()
     pipeline_output_image.save(buff, format="PNG")
     img_str = base64.b64encode(buff.getvalue())
     return {"generated_image_base64": img_str.decode('utf-8')}
 
   def finalize(self,args):
-    self.generator = None
-    
+    self.pipeline = None
